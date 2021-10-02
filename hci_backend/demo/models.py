edf3dc247c718b000a4f16867519b4c6df948ce9
Qing Shi <aliceshi0804@gmail.com>
2021-10-01 21:42:48 -0700

    add models

--- hci_backend/demo/models.py
+++ hci_backend/demo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    video_id = models.BigAutoField(primary_key=True)
+    video_path = models.CharField(max_length=200)
+    video_score = models.IntegerField()
+    related_audio = models.CharField(max_length=60)
+
+
+class Audio(models.Model):
+    audio_id = models.BigAutoField(primary_key=True)
+    audio_path = models.CharField(max_length=200)
+    audio_scroe = models.IntegerField()
+    related_video = models.CharField(max_length=60)
+    transcript = models.TextField()
+
+       
